fe7980be33317a3045e3319736623ae5aeb74100
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-05 18:45:28 -0300

    link path update

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -1,10 +1,10 @@
-import { Main } from "./MainContainerStyles/Main.Style"
-import { Header } from "./MainContainerStyles/Header.Style"
-import { BackButton } from "./MainContainerStyles/BackButton"
+import { Main } from "./MainContainer.Styles/Main.Style"
+import { Header } from "./MainContainer.Styles/Header.Style"
+import { BackButton } from "./MainContainer.Styles/BackButton.Style"
 import { BiArrowBack } from "react-icons/bi"
 import pokelogo from "../../../public/images/project/poke-logo.svg"
 import { TogglerButton } from "../ThemeToggler/TogglerButton"
-import { Section } from "./MainContainerStyles/Section.Style"
+import { Section } from "./MainContainer.Styles/Section.Style"
 import { Link, useLocation } from "react-router-dom"
 
 export const MainContainer = ({ children }) => {
@@ -13,10 +13,10 @@ export const MainContainer = ({ children }) => {
 	return (
 		<Main>
 			<Header>
-				{location.pathname === "/pokemon-DD" && (
+				{location.pathname === "/:name" && (
 					<Link to="/">
 						<BackButton title="Return to the home page">
-							<BiArrowBack />
+							<BiArrowBack /> Return to home
 						</BackButton>
 					</Link>
 				)}
