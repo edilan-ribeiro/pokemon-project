02b7fbd1ca68aca738470d650938540adb22abfa
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-05 15:30:19 -0300

    name changed

---
+++ src/components/MainContainer/MainContainer.Styles/BackButton.Style.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+import { BaseButton } from "../../GeneralButton/GeneralButton.Styles"
+
+export const BackButton = styled(BaseButton)`
+    font-size: 20px;
+    padding: 5px 15px;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    margin: 30px 50px;
+    text-align: center;
+    display: flex;
+`
---
+++ src/components/MainContainer/MainContainer.Styles/Header.Style.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Header = styled.header `
+display:flex;
+align-items:center;
+justify-content:center;
+width: 100%;
+position: relative;
+margin: 10px;
+padding: 20px;
+
+img {
+    width:450px;
+    margin-bottom: 20px;
+}
+
+@media (max-width: 450px){
+    img {
+        max-width: 350px;
+    }
+}
+`
