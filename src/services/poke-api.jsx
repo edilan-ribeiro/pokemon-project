7509a1de3b9351d5399a4957a7f01b9718e9c103
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-05 15:30:37 -0300

    parameters added

--- src/services/poke-api.jsx
+++ src/services/poke-api.jsx
@@ -1,9 +1,9 @@
-export async function pokemonList() {
-	const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")
-    return await response.json()
+export async function pokemonList(offset) {
+	const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`)
+	return await response.json()
 }
 
-export async function pokemonDetails() {
-	const response = await fetch("https://pokeapi.co/api/v2/pokemon/1/")
+export async function pokemonDetails(name) {
+	const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
 	return await response.json()
 }
