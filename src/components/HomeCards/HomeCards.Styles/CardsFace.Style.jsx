de67382617de3909ac1854a795a88793ab143e23
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-05 10:38:23 -0300

    home cards created

---
+++ src/components/HomeCards/HomeCards.Styles/CardsFace.Style.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CardsFace = styled.li`
+width: 280px;
+height: 280px;
+background-color: beige;
+padding: 20px;
+text-transform: capitalize;
+display: flex;
+flex-direction: column;
+align-items: center;
+
+`
