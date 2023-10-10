7dcc0caacb0f823079a9222b70f44ca42cae8efc
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-10 19:53:38 -0300

    removed ununsed import

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,6 +1,5 @@
 import { DetailsCard } from "../components/DetailsCard/DetailsCard"
 import { MainContainer } from "../components/MainContainer/MainContainer"
-import { pokemonDetails } from "../services/poke-api"
 
 export const DetailsPage = () => {
 	return (
