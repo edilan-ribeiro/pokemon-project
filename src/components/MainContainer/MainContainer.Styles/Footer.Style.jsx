d15d2b53cdc95173d769a073111622252f5dfdb6
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-11 18:58:21 -0300

    footer added

---
+++ src/components/MainContainer/MainContainer.Styles/Footer.Style.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+  background-color: hsl(0, 1%, 38%);
+  padding: 40px 20px;
+  text-align: center;
+  color: hsl(0, 0%, 100%);
+  width: 100%;
+
+  a {
+    color: #219aaf;
+    font-weight: 700;
+
+    &:hover{
+      text-decoration: underline;
+    }
+  }
+`
--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -6,31 +6,34 @@ import pokelogo from "../../../public/images/project/poke-logo.svg"
 import { TogglerButton } from "../ThemeToggler/TogglerButton"
 import { Section } from "./MainContainer.Styles/Section.Style"
 import { Link, useLocation } from "react-router-dom"
+import { Footer } from "./MainContainer.Styles/Footer.Style"
 
 export const MainContainer = ({ children }) => {
 	const location = useLocation()
 
 	return (
-		<Main>
-			<Header>
-				{location.pathname !== "/" && (
+		<>
+			<Main>
+				<Header>
+					{location.pathname !== "/" && (
+						<Link to="/">
+							<BackButton title="Return to the home page">
+								<BiArrowBack />
+							</BackButton>
+						</Link>
+					)}
+
 					<Link to="/">
-						<BackButton title="Return to the home page">
-							<BiArrowBack /> Back
-						</BackButton>
+						<img src={pokelogo} alt="Pokemon logo" />
 					</Link>
-				)}
+					<TogglerButton />
+				</Header>
 
-				<Link to="/">
-					<img src={pokelogo} alt="Pokemon logo" />
-				</Link>
-				<TogglerButton />
-			</Header>
-
-				<Section>
-					{children}
-				</Section>
-			
-		</Main>
+				<Section>{children}</Section>
+			</Main>
+			<Footer>
+				Made by <a href="https://edilan-ribeiro.github.io/my-portfolio/" target="_blank">Edilan Ribeiro</a>
+			</Footer>
+		</>
 	)
 }
