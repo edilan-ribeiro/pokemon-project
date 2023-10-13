aecb4d2906615a42115353d7cd6c6530ed781c43
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-12 23:03:48 -0300

    theme provider added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { GlobalStyle } from "./styles/GlobalStyle"
 import { AppRoutes } from "./routes/routes"
+import { ThemeContext, ThemeProvider} from "./contexts/ThemeContext"
+import { useContext } from "react";
+
 
 function App() {
+
+	const { theme } = useContext(ThemeContext);
+
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<GlobalStyle />
 			<AppRoutes />
-		</>
+		</ThemeProvider>
 	)
 }
 
