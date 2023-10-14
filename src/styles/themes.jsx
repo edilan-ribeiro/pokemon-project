71597b431f90417c204370c67bffbe9b41e65282
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-14 08:21:38 -0300

    small space removed

--- src/styles/themes.jsx
+++ src/styles/themes.jsx
@@ -1,23 +1,21 @@
 export const themes = {
+	light: {
+		color: "#000000",
+		background: "#F0F4F8",
+		border: "#353535",
+		buttonColor: "#95c4d6",
+		shadow: "rgba(0, 0, 0, 0.75)",
+		cardBg: "#505050",
+		cardInfo: "#e8e8e8",
+	},
 
-    light: {
-        color: '#000000',
-        background: '#F0F4F8',
-        border: '#353535',
-        buttonColor: '#95c4d6',
-        shadow: 'rgba(0, 0, 0, 0.75)',
-        cardBg: '#505050',
-        cardInfo: '#e8e8e8',
-
-    },
-
-    dark: {
-        color: '#FFFFFF',
-        background: '#202C37',
-        border: '#FFFFFF',
-        buttonColor: '#6e6c6c',
-        shadow: 'rgba(255, 255, 255, 0.75)',
-        cardBg: '#F0F4F8',
-        cardInfo: '#000000',
-    }
+	dark: {
+		color: "#FFFFFF",
+		background: "#202C37",
+		border: "#FFFFFF",
+		buttonColor: "#6e6c6c",
+		shadow: "rgba(255, 255, 255, 0.75)",
+		cardBg: "#F0F4F8",
+		cardInfo: "#000000",
+	},
 }
