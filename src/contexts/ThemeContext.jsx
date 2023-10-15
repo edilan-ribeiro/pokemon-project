42cab43b8b979bee9be02a5c5e62d52e6818a944
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-15 16:52:27 -0300

    theme now saves locally

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,13 +1,20 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 import { themes } from "../styles/themes"
-
-
 
 export const ThemeContext = createContext({})
 
 export const ThemeProvider = ({children}) => {
 
-    const [ theme, setTheme ] = useState(themes.light)
+    const getInitialTheme = () => {
+        const theme = localStorage.getItem('theme')
+        return theme ? JSON.parse(theme) : themes.light
+    }
+
+    const [ theme, setTheme ] = useState(getInitialTheme)
+
+	useEffect(() => {
+		localStorage.setItem('theme', JSON.stringify(theme));
+	  }, [theme]);
     
     return(
         <ThemeContext.Provider value={{theme, setTheme}}>
