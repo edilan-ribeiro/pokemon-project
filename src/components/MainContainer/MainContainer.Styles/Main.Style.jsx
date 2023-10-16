829ad9a57d3f637a9a8f6e1df567a3e775cc637f
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-16 10:00:00 -0300

    style update
    now app covers entire screen, removed ununsed property, paddind adjust

--- src/components/MainContainer/MainContainer.Styles/Main.Style.jsx
+++ src/components/MainContainer/MainContainer.Styles/Main.Style.jsx
@@ -1,11 +1,11 @@
 import styled from "styled-components"
 
 export const Main = styled.main`
-max-width: 1440px;
-margin: 0 auto;
-padding: 2rem;
-display:flex;
-align-items:center;
-justify-content:center;
-flex-direction: column;
+    max-width: 1440px;
+    min-height: 65vh;
+    margin: 0 auto;
+    padding: 1rem;
+    display:flex;
+    align-items:center;
+    flex-direction: column;
 `
