e84310ad0158d848cb283f6d022e941fc3749109
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-16 08:13:17 -0300

    edge case image added

--- src/utils/pokeImageUrl.js
+++ src/utils/pokeImageUrl.js
@@ -3,6 +3,8 @@ export function getImageUrl(pokemonId) {
 
 	if (pokemonId < 648) {
 		pokeImgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`
+	} else if (pokemonId === 1013) {
+		pokeImgUrl = `https://archives.bulbagarden.net/media/upload/thumb/9/98/HOME1013.png/250px-HOME1013.png`
 	} else {
 		pokeImgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`
 	}
