1fc895b0295d0d4e0ec27b90dbe317a6f2d36172
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-20 03:49:02 -0300

    Readability improved

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -2,12 +2,9 @@ import { useContext } from "react"
 import { ThemeContext } from "../../contexts/ThemeContext"
 import { DetailsOfError } from "./ErrorMessage.Styles"
 
+export const ErrorMessage = ({ children }) => {
+    
+	const { theme } = useContext(ThemeContext)
 
-export const ErrorMessage = ({children}) => {
-
-    const {theme} = useContext(ThemeContext)
-
-    return (
-        <DetailsOfError theme={theme}>{...children}</DetailsOfError>
-    )
+	return <DetailsOfError theme={theme}>{...children}</DetailsOfError>
 }
