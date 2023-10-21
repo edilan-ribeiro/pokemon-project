5bde37efc835c3263898a46be5a2a0dabc7785bb
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-21 16:12:26 -0300

    updated image and new text style

--- src/components/MainContainer/MainContainer.Styles/Header.Style.jsx
+++ src/components/MainContainer/MainContainer.Styles/Header.Style.jsx
@@ -8,10 +8,17 @@ export const Header = styled.header`
     margin: 0 auto -20px auto;
     position: relative;
     padding: 1rem;
+    flex-direction: column;
 
         img {
             width:310px;
-            margin: 50px 0;
+            margin-top: 50px;
+        }
+
+        p{            
+            margin-bottom: 50px;
+            color: ${props => props.theme.color};
+            font-size: 14px;
         }
 
         @media (min-width: 780px){
