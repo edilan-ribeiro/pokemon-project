945e98d620cbbf9cb6d8a71558a7f798d0484f84
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-10-24 19:53:29 -0300

    Test Created
    - tests the numberFill function

---
+++ src/tests/numerFill.test.js
@@ -0,0 +1,17 @@
+import { describe, expect } from "vitest"
+import { numberFill } from "../utils/numberFill"
+
+describe('Testing function to fill number space', () => {
+
+    it('should fill the empty space of a string with zeroes acording to its lenght', () => {
+       
+        expect(numberFill(5, 3)).toEqual('005')
+        expect(numberFill(7, 2)).toEqual('07')
+        expect(numberFill(150, 3)).toEqual('150')
+        expect(numberFill(25, 3)).toEqual('025')
+        expect(numberFill(95, 3)).toEqual('095')
+        expect(numberFill(1, 1)).toEqual('1')
+        expect(numberFill(1000, 5)).toEqual('01000')
+
+    })
+})
